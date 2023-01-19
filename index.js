b5ac5bd6823e393e15e74f6ba3feff77f47e2217
Aniket Kumar <kaniket0997@gmail.com>
2023-01-19 22:43:35 +0530

    defined length and path colors

---
+++ index.js
@@ -0,0 +1,10 @@
+var len=10;
+var original= 'rgb(198, 238, 245)';
+var wall= 'rgb(22, 22, 22)';
+var start_color='rgb(255, 228, 233)';
+var end='rgb(220, 130, 146)';
+var path_color='rgb(136, 189, 206)';
+var path_color2='rgb(87, 176, 211)';
+var vis_color='rgb(81, 191, 211)';
+
+
